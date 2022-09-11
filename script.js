0734194abe3baddc3ebe358bceccd10999d0c207
Eduardo <47338599+LuizEduardoMM@users.noreply.github.com>
2022-09-11 15:31:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function ingresso1() {
+
+    const ingresso1 = {id: 5520, banda: "Metallica", local: "Arena Batistao", data: "19-10-2022", valor: 500}
+document.getElementById('banda1id').innerHTML=ingresso1.id;
+    document.getElementById("banda1nome").innerHTML=ingresso1.banda;
+    document.getElementById("banda1local").innerHTML=ingresso1.local;
+    document.getElementById("banda1data").innerHTML=ingresso1.data;
+    document.getElementById("banda1valor").innerHTML=ingresso1.valor;
+}
+function ingresso2() {
+    const ingresso2 = {id: 8742, banda: "Michael Jackson", local: "Orla da Atalaia", data: "31-12-2022", valor: 200}
+    document.getElementById('banda2id').innerHTML=ingresso2.id;
+    document.getElementById("banda2nome").innerHTML=ingresso2.banda;
+    document.getElementById("banda2local").innerHTML=ingresso2.local;
+    document.getElementById("banda2data").innerHTML=ingresso2.data;
+    document.getElementById("banda2valor").innerHTML=ingresso2.valor;
+}
+function ingresso3() {
+    const ingresso3 = {id: 1498, banda: "Rolling Stones", local: "UFS", data: "31-04-2023", valor: 10000}
+    document.getElementById('banda3id').innerHTML=ingresso3.id;
+    document.getElementById("banda3nome").innerHTML=ingresso3.banda;
+    document.getElementById("banda3local").innerHTML=ingresso3.local;
+    document.getElementById("banda3data").innerHTML=ingresso3.data;
+    document.getElementById("banda3valor").innerHTML=ingresso3.valor;
+}
+
